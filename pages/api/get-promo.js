a8ce4d9e464d80407a35362321ed24d64f357583
Thayana Mamoré <thayana.mamore@topi.io>
2021-01-09 20:40:39 -0300

    Conectando a planilha ao Projeto

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -1,6 +1,30 @@
+import { GoogleSpreadsheet } from 'google-spreadsheet'
+import credentials from '../../credentials.json'
+
+const doc = new GoogleSpreadsheet('14H7WeFspFd0aCJhMd-6_3I2Q1MkTljZ7EnTD1X2jRW0')
+
 export default async(req, res) => {
-    res.end(JSON.stringify({
-        showCoupon: true,
-        message: 'Coupon message'
-    }))
+    try {
+        await doc.useServiceAccountAuth(credentials)
+        await doc.loadInfo()
+
+        const sheet = doc.sheetsByIndex[2]
+        await sheet.loadCells('A3:B3')
+
+        const mostrarPromocaoCell = sheet.getCell(2, 0)
+        const MsgPromocaoCell = sheet.getCell(2, 1)
+        
+        res.end(JSON.stringify({
+            showCoupon: mostrarPromocaoCell.value === 'VERDADEIRO',
+            message: MsgPromocaoCell.value
+        }))
+
+    } catch (err) {
+        console.log(err)
+        res.end(JSON.stringify({
+            showCoupon: false,
+            message: ''
+        }))
+    }
+
 }
--- pages/api/save.js
+++ pages/api/save.js
@@ -1,5 +1,29 @@
+import { GoogleSpreadsheet } from 'google-spreadsheet'
+import credentials from '../../credentials.json'
+
+const doc = new GoogleSpreadsheet('14H7WeFspFd0aCJhMd-6_3I2Q1MkTljZ7EnTD1X2jRW0')
+
 export default async(req, res) => {
-    console.log(JSON.parse(req.body))
-    // JSON.parse() tranforma em objeto
-    res.end(req.body)
+    try {
+        await doc.useServiceAccountAuth(credentials)
+        await doc.loadInfo()
+
+        const sheet = doc.sheetsByIndex[1]
+        const data = JSON.parse(req.body)
+        console.log(data)
+
+        await sheet.addRow({
+            Nome: data.Nome,
+            Email: data.Email,
+            Whatsapp: data.Whatsapp,
+            Cupom: 'aaa111',
+            Promo: 'asdfasdf',
+        })
+
+        res.end(req.body)        
+
+    } catch (err) {
+        console.log(err)
+        res.end('error')
+    }
 }
