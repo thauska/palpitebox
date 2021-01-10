7d1df3c88b235a3a1bb56f6d72e83174b385dd23
Thayana Mamoré <thayana.mamore@topi.io>
2021-01-10 20:47:27 -0300

    Moment date e pegando infos da promoção

--- pages/api/save.js
+++ pages/api/save.js
@@ -1,5 +1,6 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet'
 import credentials from '../../credentials.json'
+import moment from 'moment'
 
 const doc = new GoogleSpreadsheet('14H7WeFspFd0aCJhMd-6_3I2Q1MkTljZ7EnTD1X2jRW0')
 
@@ -12,12 +13,28 @@ export default async(req, res) => {
         const data = JSON.parse(req.body)
         console.log(data)
 
+        const sheetConfig = doc.sheetsByIndex[2]
+        await sheetConfig.loadCells('A3:B3')
+
+        const mostrarPromocaoCell = sheetConfig.getCell(2, 0)
+        const MsgPromocaoCell = sheetConfig.getCell(2, 1)
+
+        let Cupom = ''
+        let Promo = ''
+        if(mostrarPromocaoCell.value === 'VERDADEIRO') {
+            // TODO: gerar cupom
+            Cupom = 'temporario'
+            Promo = MsgPromocaoCell.value
+        }
+
         await sheet.addRow({
             Nome: data.Nome,
             Email: data.Email,
             Whatsapp: data.Whatsapp,
-            Cupom: 'aaa111',
-            Promo: 'asdfasdf',
+            Nota: 5,
+            'Data Preenchimento': moment().format('DD/MM/YYYY, HH:mm'),
+            Cupom,
+            Promo
         })
 
         res.end(req.body)        
