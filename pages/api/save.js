aa2a20bee69cc837d6b34a511688cd42d6ae0d47
Thayana Mamoré <thayana.mamore@topi.io>
2021-01-10 20:56:07 -0300

    Gerando o cupom de forma simples

--- pages/api/save.js
+++ pages/api/save.js
@@ -4,6 +4,11 @@ import moment from 'moment'
 
 const doc = new GoogleSpreadsheet('14H7WeFspFd0aCJhMd-6_3I2Q1MkTljZ7EnTD1X2jRW0')
 
+const genCupom = () => {
+    const code = parseInt(moment().format('YYMMDDHHmmssSSSS')).toString(16).toUpperCase()
+    return code.substr(0, 4) + '-' + code.substr(4, 4) + '-' + code.substr(8, 4)
+}
+
 export default async(req, res) => {
     try {
         await doc.useServiceAccountAuth(credentials)
@@ -23,7 +28,7 @@ export default async(req, res) => {
         let Promo = ''
         if(mostrarPromocaoCell.value === 'VERDADEIRO') {
             // TODO: gerar cupom
-            Cupom = 'temporario'
+            Cupom = genCupom()
             Promo = MsgPromocaoCell.value
         }
 
@@ -32,7 +37,7 @@ export default async(req, res) => {
             Email: data.Email,
             Whatsapp: data.Whatsapp,
             Nota: 5,
-            'Data Preenchimento': moment().format('DD/MM/YYYY, HH:mm'),
+            'Data Preenchimento': moment().format('DD/MM/YYYY, HH:mm:ss'),
             Cupom,
             Promo
         })
