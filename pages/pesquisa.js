804cbe42a492690df17ca2440eb6ee4e7ca07e73
Thayana Mamoré <thayana.mamore@topi.io>
2021-01-10 21:10:16 -0300

    Formulário para coletar dados dentro da planilha

--- pages/pesquisa.js
+++ pages/pesquisa.js
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 
 const Pesquisa = () => {
 
+    const [form, setForm] = useState({
+        Nome: '',
+        Email: '',
+        Whatsapp: ''
+    })
+
     const save = async () => {
-        const form = {
-            Nome: 'aaa',
-            Email: 'bbb',
-            Whatsapp: 'ccc'
-        }
 
         try{
             const response = await fetch('/api/save', {
@@ -24,6 +25,17 @@ const Pesquisa = () => {
 
     }
 
+    const onChange = event => {
+        console.log(event.target.value)
+
+        const value = event.target.value
+        const key = event.target.name
+        setForm(old => ({
+            ...old,
+            [key]: value
+        }))
+    }
+
     return (
         <div>
             <h1 className="text-center font-bold my-4 text-3xl">Críticas e sugestões</h1>
@@ -33,8 +45,16 @@ const Pesquisa = () => {
             </p>
             <div className="w-1/5 mx-auto">
                 <label className="font-bold">Seu nome:</label>
-                <input type="text" className="p-4 my-2 block shadow bg-blue-100 rounded" />
+                <input type="text" className="p-4 my-2 block shadow bg-blue-100 rounded" placeholder="Nome" onChange={onChange} name="Nome" value={form.Nome} />
+                <label className="font-bold">E-mail:</label>
+                <input type="text" className="p-4 my-2 block shadow bg-blue-100 rounded" placeholder="E-mail" onChange={onChange} name="Email" value={form.Email} />
+                <label className="font-bold">Whatsapp:</label>
+                <input type="text" className="p-4 my-2 block shadow bg-blue-100 rounded" placeholder="Whatsapp" onChange={onChange} name="Whatsapp" value={form.Whatsapp} />
                 <button className="bg-blue-400 px-12 py-4 font-bold rounded-xl hover:bg-blue-500" onClick={save}>Salvar</button>
+
+                <pre>
+                    {JSON.stringify(form, null, 2)}
+                </pre>
             </div>
         </div>
     )
