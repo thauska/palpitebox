649cde9a772401b8bb0332b675a9633831f829e1
Thayana Mamoré <thayana.mamore@topi.io>
2021-01-10 23:19:12 -0300

    criando pasta utils para base64

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -1,11 +1,7 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet'
 
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
-
-const fromBase64 = value => {
-    const buff = new Buffer.from(value, 'base64');
-    return buff.toString('ascii');
-}
+import { fromBase64 } from '../../utils/base64'
 
 export default async(req, res) => {
     try {
